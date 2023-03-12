2d7849c9fa1e7858963917341213faa8a2edbe46
Frank <frank627916868@gmail.com>
2023-03-13 02:04:58 +0800

    feat: update websocket communication

--- server.py
+++ server.py
@@ -1,13 +1,33 @@
 import asyncio
 import websockets
+from fight import Fight
+import json
 
-async def echo(websocket, path):
+
+async def echo(websocket: websockets, path):
 
     client_address = websocket.remote_address
-
+    
     async for message in websocket:
-        print(f"Received message from {client_address[1]}: {message}")
-        await websocket.send(str(client_address))
+        if message == "start":
+            started = True
+            fight = Fight()
+
+            print(f"Received message from {client_address[1]}: {message}")
+            print(fight.get_board())
+            value = fight.get_board()
+            await websocket.send(json.dumps((value)))
+            
+        elif started:
+
+            return_message = fight.play(message)
+            if fight.game_end() != False:
+                started = False
+                await websocket.send(json.dumps(fight.game_end()))
+            else:
+                await websocket.send(json.dumps((return_message)))
+                
+
 
 async def main():
     async with websockets.serve(echo, "localhost", 8000):
